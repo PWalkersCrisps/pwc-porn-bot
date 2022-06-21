9f723076ca0ad08e565c42a33d49f34ddddad9bc
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2022-06-21 19:17:30 +0100

    bug fixes

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-inner-declarations */
 const { CLIENT_ID, GUILD_ID } = require('../arrays/config.json');
+const { MessageEmbed } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const randomPornTopic = require('../arrays/randomTopic.js');
-const { randomInt } = require('../modules/random.js');
 const commands = require('../arrays/interactionCommands.js');
 require('dotenv').config();
-const { MessageEmbed } = require('discord.js');
+
+const { randomInt } = require('../modules/random.js');
+const randomPornTopic = require('../arrays/randomTopic.js');
+const akaneko = require('akaneko');
+
+const Reddit = require('reddit');
+const reddit = new Reddit({
+	username: String(process.env.REDDIT_USERNAME),
+	password: String(process.env.REDDIT_PASSWORD),
+	appId: String(process.env.REDDIT_APP_ID),
+	appSecret: String(process.env.REDDIT_APP_SECRET),
+	userAgent: 'MyApp/1.0.0 (http://example.com)',
+});
 
 module.exports = {
 	name: 'ready',
@@ -28,23 +39,25 @@ module.exports = {
 
 			console.log('Successfully reloaded application (/) commands.');
 
-			function foo() {
-				try {
-					setTimeout(async function() {
-						const akanekoSan = new MessageEmbed()
-							.setColor('RANDOM')
-							.setImage(await randomPornTopic[randomInt(0, randomPornTopic.length)]);
+			function post() {
+				setTimeout(async function() {
+					const topic = await randomPornTopic[randomInt(0, randomPornTopic.length)] || await akaneko.nsfw.hentai();
+
+					const akanekoSan = new MessageEmbed()
+						.setColor('RANDOM')
+						.setTimestamp()
+						.setDescription(String(topic.text))
+						.setImage(await topic.image);
+					try {
 						client.channels.fetch('988549632500039711').then(channel => channel.send({ embeds: [akanekoSan] }));
-						foo();
-					}, 15 * 1000);
-				}
-				catch (error) {
-					console.error(error);
-				}
-
+					}
+					catch (error) {
+						console.error(error);
+					}
+					post();
+				}, 30 * 1000);
 			}
-
-			foo();
+			post();
 		}
 		catch (error) {
 			console.error(error);
