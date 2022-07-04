2be3c8f3d5552b16ed569c3b49f0523348fe14ea
Ashley Walker <dev@pwalkerscrisps.com>
2022-07-04 15:06:37 +0000

    Deleted commands not needed

--- src/arrays/interactionCommands.js
+++ src/arrays/interactionCommands.js
@@ -2,45 +2,41 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = [
 	new SlashCommandBuilder()
-		.setName('ass')
-		.setDescription('nsfw'),
-	new SlashCommandBuilder()
-		.setName('bdsm')
-		.setDescription('nsfw'),
-	new SlashCommandBuilder()
-		.setName('cum')
-		.setDescription('nsfw'),
-	new SlashCommandBuilder()
-		.setName('doujin')
-		.setDescription('nsfw'),
-	new SlashCommandBuilder()
-		.setName('femdom')
-		.setDescription('nsfw'),
-	new SlashCommandBuilder()
-		.setName('hentai')
-		.setDescription('nsfw'),
-	new SlashCommandBuilder()
-		.setName('maid')
-		.setDescription('nsfw'),
-	new SlashCommandBuilder()
-		.setName('neko')
-		.setDescription('nsfw'),
-	new SlashCommandBuilder()
-		.setName('orgy')
-		.setDescription('nsfw'),
-	new SlashCommandBuilder()
-		.setName('panty')
-		.setDescription('nsfw'),
-	new SlashCommandBuilder()
-		.setName('ass')
+		.setName('booru')
 		.setDescription('nsfw')
-		.addStringOption(option => option.setName('tag').setDescription('Search with R34 tag').setRequired(true)),
+		.addStringOption(option =>
+			option.setName('boorus')
+				.setDescription('Choose specific boorus')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'e621', value: 'e621' },
+					{ name: 'e926', value: 'e926' },
+					{ name: 'hypnohub', value: 'hypnohub' },
+					{ name: 'danbooru', value: 'danbooru' },
+					{ name: 'konachan', value: 'konachan' },
+					{ name: 'yande.re', value: 'yandere' },
+					{ name: 'gelbooru', value: 'gelbooru' },
+					{ name: 'rule34', value: 'rule34' },
+					{ name: 'safebooru', value: 'safebooru' },
+					{ name: 'tbib', value: 'tbib' },
+					{ name: 'xbooru', value: 'xbooru' },
+					{ name: 'rule34paheal', value: 'rule34paheal' },
+					{ name: 'derpibooru', value: 'derpibooru' },
+					{ name: 'realbooru', value: 'realbooru' },
+				))
+		.addStringOption(option =>
+			option.setName('tags')
+				.setDescription('Tags to search for')
+				.setRequired(true)),
+
 	new SlashCommandBuilder()
 		.setName('botinfo')
 		.setDescription('Get information about the bot'),
+
 	new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Ping the server the bot is being hosted on and see the delays that there is'),
+
 	new SlashCommandBuilder()
 		.setName('uptime')
 		.setDescription('See how long the bot has been up for since startup'),
