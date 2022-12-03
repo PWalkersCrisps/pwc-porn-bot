e087fad082d7a3ab7c17730b972964a8e1cafe45
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2022-12-03 19:53:28 +0000

    remove build

--- build/models/guildSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const mongoose_1 = require("mongoose");
-module.exports = (0, mongoose_1.model)('guilds', new mongoose_1.Schema({
-    guildID: { type: mongoose_1.Schema.Types.String, require: true, unique: true },
-    registeredAt: { type: mongoose_1.Schema.Types.Date, default: Date.now() },
-    premium: { type: mongoose_1.Schema.Types.Boolean, default: false },
-    autoPostChannel: { type: mongoose_1.Schema.Types.String, default: null },
-}));
--- build/modules/globalVariables.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const date = new Date().toLocaleDateString();
-const time = new Date().toLocaleTimeString();
-const currentDate = date + ' @ ' + time;
-module.exports = { currentDate };
