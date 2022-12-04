f46ab52bcaa02b33fb18412b160803af67818433
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2022-12-04 22:11:41 +0000

    Reduce Loop Delay

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -31,7 +31,7 @@ module.exports = {
 
             console.log(`${ time.currentDate } Successfully reloaded application (/) commands.`);
 
-            autoPostBooru.postToPremiumServer(client, 10);
+            autoPostBooru.postToPremiumServer(client, 60);
         }
         catch (error) {
             console.error(`${time.currentDate} ready error: ${error}`);
