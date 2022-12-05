1ef327de4939daa9b9219d2fae1b4fe5de22a3b5
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2022-12-05 08:33:22 +0000

    Restrict Echo Access

--- src/commands/debug/echo.ts
+++ src/commands/debug/echo.ts
@@ -1,13 +1,21 @@
 import { EmbedBuilder } from 'discord.js';
+
+const allowedRoles = [
+    '811018391913627688',
+    '816349466008748033',
+];
+
 module.exports = {
     name: 'echo',
     description: 'Echo your message',
     async execute(client: any, interaction: any) {
-        if (!(interaction.user.id === '426455031571677197')) return interaction.reply({ content: `<@${interaction.user.id}> actually have permissions to use the command next time`, ephemeral: true });
+        if (!(interaction.member.roles.cache.some((role: any) => allowedRoles.includes(role.id)) || interaction.user.id === '426455031571677197')) {
+            return interaction.reply({ content: `${interaction.user} actually have permissions to use the command next time`, ephemeral: false });
+        }
 
         const string = interaction.options.getString('input');
 
-        await interaction.reply({ content: `${string}` });
+        interaction.reply({ content: `${string}` });
 
     },
 };
