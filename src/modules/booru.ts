6cd58b33d37633764d486f434a2afa020a156475
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2022-12-05 08:35:56 +0000

    Update booru.ts

--- src/modules/booru.ts
+++ src/modules/booru.ts
@@ -5,7 +5,7 @@ export = {
     search: async function(site: string, tags: string | string[], random = true): Promise<void | BooruPost> {
         try {
             const searchResults: SearchResults = await search(site, tags, { limit: 1, random });
-            const post = this.tagFilter(searchResults[0] as BooruPost);
+            const post = this.tagFilter(searchResults[0] as BooruPost, { site, tags, random });
 
             if (!post) {
                 return this.search(site, tags, random);
@@ -17,11 +17,12 @@ export = {
             return console.error(error);
         }
     },
-    tagFilter: function(post: BooruPost): BooruPost | undefined {
+    tagFilter: function(post: BooruPost, postOptions: { site: string, tags: string | string[], random: boolean }): BooruPost | undefined {
 
         const blacklistedTags: string[] = filters;
 
         if (!(post)) {
+            this.search(postOptions.site, postOptions.tags, postOptions.random);
             return;
         }
 
@@ -33,9 +34,14 @@ export = {
             }
         }
 
+        if (blacklisted) {
+            this.search(postOptions.site, postOptions.tags, postOptions.random);
+            return;
+        }
+
         return post;
     },
-    guildFilter: async function(guildID: string, post: BooruPost): Promise<BooruPost | undefined> {
+    guildFilter: async function(guildID: string, post: BooruPost, postOptions: { site: string, tags: string | string[], random: boolean }): Promise<BooruPost | undefined> {
 
         const guild: GuildDocument | null = await guildSchema.findOne({ guildID: guildID });
 
@@ -51,6 +57,16 @@ export = {
             }
         }
         if (blacklisted) {
+            this.search(postOptions.site, postOptions.tags, postOptions.random);
+            return;
+        }
+
+        return post;
+    },
+    ratingFilter: function(post: BooruPost, rating: string, postOptions: { site: string, tags: string | string[], random: boolean }): BooruPost | undefined {
+
+        if (post.rating !== rating) {
+            this.search(postOptions.site, postOptions.tags, postOptions.random);
             return;
         }
 
