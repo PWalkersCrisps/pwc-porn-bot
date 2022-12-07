6d0840e1ffccc885334ab040725013744b91d66f
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2022-12-07 13:35:26 +0000

    Overhaul booru

--- src/modules/booru.ts
+++ src/modules/booru.ts
@@ -1,71 +1,42 @@
 import { search, SearchResults } from 'booru';
 import filters from '../data/filters.json';
 import guildSchema from '../models/guildSchema';
+
 export = {
-    search: async function(site: string, tags: string | string[], random = true): Promise<void | BooruPost> {
+    searchPosts: async function(site: string, tags: string | string[], limit = 1, random = true, rating: string[] = ['e', 'q']): Promise<void | BooruPost> {
         try {
-            const searchResults: SearchResults = await search(site, tags, { limit: 1, random });
-            const post = this.tagFilter(searchResults[0] as BooruPost);
+            search(site, tags, { limit, random })
+                .then((posts) => {
+                    const filteredPosts = posts.filter((post) => { // Filter out any posts with the specified tags
+                        return !filters.some((tag) => post.tags.includes(tag));
+                    });
 
-            if (!post) {
-                return;
-            }
+                    filteredPosts.filter((post) => {
+                        return rating.includes(post.rating);
+                    });
 
-            return post;
+                    if (!filteredPosts) return; // If there are no posts left, return from the function
+
+                    return filteredPosts; // Otherwise, return the filtered posts
+                })
+                .then((posts) => {
+
+                    if (!posts) {// If there are no posts left, rerun the function
+
+                        this.searchPosts(site, tags, limit, random, rating);
+                        return;
+                    }
+
+                    const post = posts[0];
+                    return post;
+                })
+                .catch((error) => {
+                    // Handle any errors
+                    console.error(error);
+                });
         }
         catch (error) {
             return console.error(error);
         }
-    },
-    tagFilter: function(post: BooruPost): BooruPost | undefined {
-
-        const blacklistedTags: string[] = filters;
-
-        if (!(post)) {
-            return;
-        }
-
-        // check if post has any blacklisted tags
-        let blacklisted = false;
-        for (const tag of blacklistedTags) {
-            if (post.tags.includes(tag)) {
-                blacklisted = true;
-            }
-        }
-
-        if (blacklisted) {
-            return;
-        }
-
-        return post;
-    },
-    guildFilter: async function(guildID: string, post: BooruPost): Promise<BooruPost | undefined> {
-
-        const guild: GuildDocument | null = await guildSchema.findOne({ guildID: guildID });
-
-        if (!guild) {
-            return;
-        }
-
-        // check if post has any blacklisted tags
-        let blacklisted = false;
-        for (const tag of guild.tagBlacklist) {
-            if (post.tags.includes(tag)) {
-                blacklisted = true;
-            }
-        }
-        if (blacklisted) {
-            return;
-        }
-
-        return post;
-    },
-    ratingFilter: function(post: BooruPost, rating: string): BooruPost | undefined {
-
-        if (post.rating !== rating) {
-            return;
-        }
-
-        return post;
     },
 }
